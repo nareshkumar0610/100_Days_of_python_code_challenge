8fc5037792079b18b4cbdc0881aa3ef4ebe4dc93
nareshkumar0610 <nareshkumar061097.nk@gmail.com>
2024-06-08 23:25:29 +0530

    Update Random Password generator.py

--- Day-5 For Loops and Range/Random Password generator.py
+++ Day-5 For Loops and Range/Random Password generator.py
@@ -7,7 +7,7 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-#Step_1: Ask user fro inputs
+#Step_1: Ask user for inputs
 print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
